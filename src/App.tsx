5499ddb83322a477cb01173b58b8cee8003da982
Nico <nicolas.serrato@outlook.com>
2020-12-01 08:46:01 -0300

    Github api explorer fineshed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Mundão</h1>
-    </div>
-  );
-}
+import GlobalStyle from './styles/global';
+import Routes from './routes';
 
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
+);
 export default App;
